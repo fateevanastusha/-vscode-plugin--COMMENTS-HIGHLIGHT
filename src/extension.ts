740fb049e516d3904b0978cde7800f73771202a0
anastasiafateeva <a.fateeva@energo.app>
2025-04-26 23:40:03 +0300

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,29 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  const highlightDecoration = vscode.window.createTextEditorDecorationType({
-    color: '#FF8800',
-    fontWeight: 'bold',
-  });
+  function createHighlightDecoration(): vscode.TextEditorDecorationType {
+    const config = vscode.workspace.getConfiguration('commentHighlighter');
+    let color = config.get<string>('color', '#ff5794') || '#ff5794';
+    if (color[0] !== '#') {
+      color = `#${color}`;
+    }
+    return vscode.window.createTextEditorDecorationType({
+      color: color,
+      fontWeight: 'bold',
+    });
+  }
+
+  let highlightDecoration = createHighlightDecoration();
 
   const updateDecorations = (editor: vscode.TextEditor) => {
     if (!editor) {
       return;
     }
-    const regEx = /(\/\/\s*TODO:.*$)/gm;
+    const regEx = /\/\/.*$|\/\*[\s\S]*?\*\//gm;
     const text = editor.document.getText();
     const decorations: vscode.DecorationOptions[] = [];
-
     let match;
+
     while ((match = regEx.exec(text))) {
       const startPos = editor.document.positionAt(match.index);
       const endPos = editor.document.positionAt(match.index + match[0].length);
@@ -42,6 +51,20 @@ export function activate(context: vscode.ExtensionContext) {
       const editor = vscode.window.activeTextEditor;
       if (editor && event.document === editor.document && isSupportedLanguage(editor.document.languageId)) {
         updateDecorations(editor);
+      }
+    }),
+
+    vscode.workspace.onDidChangeConfiguration((event) => {
+      if (event.affectsConfiguration('commentHighlighter.color')) {
+        if (highlightDecoration) {
+          highlightDecoration.dispose();
+        }
+        highlightDecoration = createHighlightDecoration();
+
+        const editor = vscode.window.activeTextEditor;
+        if (editor && isSupportedLanguage(editor.document.languageId)) {
+          updateDecorations(editor);
+        }
       }
     }),
   );
